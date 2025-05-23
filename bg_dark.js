c9d072b4e5a6e6a8f3b37b367d6e75ef2f4bbf5c
ooPERYTONoo <114829926+ooPERYTONoo@users.noreply.github.com>
2025-05-23 17:48:39 +0530

    Delete bg_dark.js

--- bg_dark.js
+++
@@ -1,8 +0,0 @@
-
-{/* <link rel="stylesheet" href="birthdayOverlay.css" /> */ }
-
-const script = document.createElement("script");
-script.src = "happyBirthday.js";
-script.id = "birthdayScript";
-document.body.appendChild(script);
-
